78700163d5cbdefcafb9bed895cbe82bce9b0bb0
Trần Minh Quang <tmquang0209@gmail.com>
2024-01-13 16:28:17 +0700

    Add authentication and dropdown menu to Navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+// Note: Navbar component
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+
+// import icons
 import { FaCaretDown } from "react-icons/fa";
 import { LuShoppingCart } from "react-icons/lu";
+import { IoMenuOutline } from "react-icons/io5";
+
+// import components
 import { Modal } from "../Modal";
 import LoginForm from "./loginForm";
 
@@ -32,8 +39,17 @@ const DropdownLinks = [
     },
 ];
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
     const [isOpen, setIsOpen] = React.useState(false);
+    const [accessToken, setAccessToken] = useState(null);
+
+    useEffect(() => {
+        console.log(auth);
+        if (auth.user?.token) {
+            setAccessToken(auth.user.token);
+        }
+    }, [auth]);
 
     return (
         <>
@@ -83,12 +99,70 @@ const Navbar = () => {
                                     </button>
                                 </li>
                                 {/* account */}
-                                <li className="inline-block px-4 py-4 duration-200 hover:text-primary">
-                                    <button className="" onClick={() => setIsOpen(true)}>
-                                        Login
-                                    </button>
-                                </li>
+                                {!accessToken ? (
+                                    <li className="inline-block px-4 py-4 duration-200 hover:text-primary">
+                                        <button className="" onClick={() => setIsOpen(true)}>
+                                            Login
+                                        </button>
+                                    </li>
+                                ) : (
+                                    <li className="group relative cursor-pointer">
+                                        <a href="/profile" className="gap[2px] flex h-[72px] items-center">
+                                            {auth.user?.first_name + " " + auth.user?.last_name}
+                                            <span>
+                                                <FaCaretDown className="transition duration-300 group-hover:rotate-180" />
+                                            </span>
+                                        </a>
+                                        {/* dropdown link section */}
+                                        <div className="absolute -left-1 z-[10] hidden w-[120px] bg-white p-2 text-black shadow-md group-hover:block">
+                                            <ul>
+                                                <li>
+                                                    <a href="/orders" className="inline-block w-full rounded-md p-2 hover:bg-primary/20">
+                                                        Orders
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="/logout" className="inline-block w-full rounded-md p-2 text-red-600 hover:bg-primary/20">
+                                                        Logout
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                )}
                             </ul>
+                            <div className="sm:hidden">
+                                <ul>
+                                    <li className="group">
+                                        <a href="/#" className="gap[2px] flex h-[72px] items-center text-2xl">
+                                            <IoMenuOutline />
+                                        </a>
+                                        <div className="absolute -left-0 z-[10] hidden w-full bg-white p-2 text-black shadow-md group-hover:block">
+                                            <ul>
+                                                {Menu.map((item, index) => (
+                                                    <li key={item.id}>
+                                                        <a href={item.link} className="inline-block px-4 py-4 duration-200 hover:text-primary">
+                                                            {item.name}
+                                                        </a>
+                                                    </li>
+                                                ))}
+                                                <li className="group relative cursor-pointer">
+                                                    <a href="/#" className="gap[2px] flex h-[72px] items-center">
+                                                        Quick links
+                                                    </a>
+                                                    {DropdownLinks.map((item, index) => (
+                                                        <li key={index}>
+                                                            <a href={item.link} className="inline-block w-full rounded-md p-2 hover:text-primary">
+                                                                {item.name}
+                                                            </a>
+                                                        </li>
+                                                    ))}
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -98,4 +172,8 @@ const Navbar = () => {
     );
 };
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+});
+
+export default connect(mapStateToProps, {})(Navbar);
