bb992b82a14c1d068d2ae1966962dc8425a9388a
Trần Minh Quang <tmquang0209@gmail.com>
2024-01-16 10:23:42 +0700

    Update API URLs

--- src/API/url.js
+++ src/API/url.js
@@ -2,12 +2,16 @@ const host = "https://book-store-bqe8.onrender.com";
 
 const url = {
     // User
+    authToken: `${host}/user/getUserByToken`,
     login: `${host}/user/login`,
-    signup: `${host}/user/signup`,
+    signup: `${host}/user/register`,
     // banner
     getBanner: `${host}/banner`,
+    // category
+    getCategory: `${host}/category`,
+
     // product
-    getProduct: `${host}/product/allProducts`,
+    getProduct: `${host}/product`,
     getProductById: `${host}/product/productById`,
     getProductByCategory: `${host}/product/productByCategory`,
     getTrendingProducts: `${host}/product/trending`,
